26c6deb39e5c792a35f1ae4845b2c03292a0aa09
adithyakandala <kandalaadithya@gmail.com>
2021-06-11 16:15:01 +0530

    third commit

--- pages/newmeetup/index.js
+++ pages/newmeetup/index.js
@@ -1,8 +1,9 @@
+import { useRouter } from "next/router"
 import NewMeetupForm from '../../components/meetups/NewMeetupForm'
 
 function Newmeetup(){
   async function  formhandler(meetsups){
-  
+  const router = useRouter()
      const response = await fetch('/api/new-meetup',{ method:'POST', body:JSON.stringify(meetsups),headers:{
          'Content-Type':'application/json'
        }
@@ -11,6 +12,9 @@ function Newmeetup(){
 const data =await response.json()
 
 console.log(data)
+
+router.push('/')
+
 
 }
     return(
