2cb100bf4da9bb132ae4f6974faaa6a8ed04a653
adithyahemanth <45818061+adithyahemanth@users.noreply.github.com>
2021-09-22 20:37:24 +0530

    Update MainNavigation.js

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -4,7 +4,7 @@ function MainNavigation() {
 
   return (
     <header className={classes.header}>
-      <div className={classes.logo}><Link href='/'>React Meetups</Link></div>
+      <div className={classes.logo}><Link href='/'>Nature Meetups</Link></div>
       <nav>
         <ul>
           <li>
